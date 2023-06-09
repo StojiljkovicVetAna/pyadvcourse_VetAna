9a00b43eab2315bca175f1c6ec95f56ffa7a2821
StojiljkovicVetAna <25721415+StojiljkovicVetAna@users.noreply.github.com>
2023-06-09 16:23:57 +0200

    wrong_add_four

--- src/pyadvcourse_VetAna/algos.py
+++ src/pyadvcourse_VetAna/algos.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def add_four(x):
-    return x + 4
+    return x + 5
 
 def add_three(x):
     return x + 3
--- tests/test_pyadvcourse_VetAna.py
+++ tests/test_pyadvcourse_VetAna.py
@@ -1,2 +1,4 @@
-def test_something():
-    pass
+from pyadvcourse_VetAna import algos
+
+def test_add_four():
+    assert algos.add_four(4) == 8
